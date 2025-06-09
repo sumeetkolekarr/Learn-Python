042bdb7ef85adf7f6ee8d5ef56e05340c42cbc5d
Sumeet Kolekarr <sumeetkolekarr555@gmail.com>
2025-06-09 16:00:51 +0530

    Started Learning Python

---
+++ Comments and Variables/index.py
@@ -0,0 +1,12 @@
+# Comments
+# Hello, I own a Cat
+"""I am Sumeet
+I am studying Python"""
+
+# Variables
+name='SK'
+num = 555
+
+HelloWorld = 'Hi' #Pascal Case
+helloWorld = 'Hi' #Camel Case
+hello_world = 'Hi' #Snake Case
---
+++ Data Types and Conversions/index.py
@@ -0,0 +1,46 @@
+# Numbers
+# num = 555
+# dec = 52.5
+# frac = 12/3
+# comp = 5j
+# print(type(num))
+# print(type(dec))
+# print(type(frac))
+# print(type(comp))
+
+# Strings
+# char = '5645464545'
+# print(type(char))
+
+# Boolean
+# bl = True
+# print(type(bl))
+
+# Conversions
+a = 65
+
+# Conversion from Unicode to Character
+print(chr(a))
+
+# Indexing
+name = 'Sumeet'
+statement = 'My name is Sumeet'
+print(name[5],name[-1])
+print(statement[0:3:1])
+print(statement[::])
+
+# TypeCasting or TypeConversion
+a = '12'
+a=int(a)
+print(type(a))
+print(bool(a))
+
+# False, 0, 0.0, '', [], () and {} provides false on converting these in boolean
+
+# String Formatting
+name='SK'
+age=22
+print(f"My name is {name} and my age is {age}")
+
+inpAge = input('Enter your Age')
+print(inpAge)
