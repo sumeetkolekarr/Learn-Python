5904c0c054c58cbd9e0ed2c514135711635dfc4f
Sumeet Kolekarr <sumeetkolekarr555@gmail.com>
2025-06-12 15:25:29 +0530

    Operators Completed

---
+++ Operators/index.py
@@ -0,0 +1,39 @@
+a = 5
+b = 32
+
+# Arithmetic Operators
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a%b) 
+# print(a/b) 
+# print(a//b) 
+# print(a**2) 
+
+# Assignment Operators
+c = 20
+
+# Compound Assignment Operators
+# c +=40
+# c -=40
+# c *= 5
+# c /= 2
+# c //= 2
+# c **= 2
+# print(c)
+
+# Comparison Operators
+# print(a==b)
+# print(a!=b)
+# print(a>=b)
+# print(a<=b)
+# print(a<b)
+# print(a>b)
+
+# Comparing ASCII Values
+# print('A'>'B')
+
+# Logical Operators
+print(123>100 and 34==34 and 45<90 and 12>20)
+print(123>100 or 34==34 or 45<90 or 12>20)
+print(not(123>100 or 34==34 or 45<90 or 12>20))
