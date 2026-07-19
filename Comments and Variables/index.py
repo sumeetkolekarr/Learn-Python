3bfdf71f47a6c3630226672b40986767bb8ad310
Sumeet Kolekarr <sumeetkolekarr555@gmail.com>
2026-07-19 22:19:08 +0530

    Recapping concepts

--- Comments and Variables/index.py
+++ Comments and Variables/index.py
@@ -7,6 +7,7 @@ I am studying Python"""
 name='SK'
 num = 555
 
+# Can not use numbers at start, can not use spaces in between and also can't use any special character
 HelloWorld = 'Hi' #Pascal Case
 helloWorld = 'Hi' #Camel Case
 hello_world = 'Hi' #Snake Case
